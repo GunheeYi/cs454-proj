95fe2e0a119ec30dddb27428cd3e8023f1ac965a
3-24 <dev.youngseok@gmail.com>
2021-11-21 21:21:11 +0900

    Minor improvement

--- simulate.py
+++ simulate.py
@@ -1,40 +1,43 @@
-from config import *
-from beamngpy import BeamNGpy, Scenario, Vehicle, setup_logging, Road
+from config import BNG_HOME, BNG_USER
+from beamngpy import BeamNGpy, Scenario, Vehicle, Road
 from beamngpy.sensors import Electrics
 import time
 
 def main():
-    #setup_logging()
-    with BeamNGpy('localhost', 64256, home=BNG_HOME, user=BNG_USER) as bng:
-        bng.set_steps_per_second(60)    #60 fps
-        bng.set_deterministic()
-        scenario = Scenario('tig', 'test_scenario')
-        ground_level = -28.0
-        road_nodes = [
+    scenario = Scenario('tig', 'test_scenario')
+    ground_level = -28.0
+
+    # Create Road
+    road_nodes = [
             (-100, 0, ground_level, 8),
             (  0, 0, ground_level, 8),
             (+100, 0, ground_level, 8)
         ]
-        road = Road('tig_road_rubber_sticky', rid='straight_road')
-        road.nodes.extend(road_nodes)
-        scenario.add_road(road)
-        
-        direction_of_the_road = (0, 0, 1, -1)
-        ego_position = (-80.0, -2.0, ground_level)
-        ego_vehicle = Vehicle('ego', model='etk800', licence='ego', color="white")
-        electrics = Electrics()
-        ego_vehicle.attach_sensor('electrics', electrics)
-        scenario.add_vehicle(ego_vehicle, pos=ego_position, rot=None, rot_quat=direction_of_the_road)
-        parked_car_position = (0.0, -2.0, ground_level)
-        parked_vehicle = Vehicle('parked', model='etk800', licence='parked', color="red")
-        scenario.add_vehicle(parked_vehicle, pos=parked_car_position, rot=None, rot_quat=direction_of_the_road)
+    road = Road('tig_road_rubber_sticky', rid='straight_road')
+    road.nodes.extend(road_nodes)
+    scenario.add_road(road)
+    direction_of_the_road = (0, 0, 1, -1)
 
+    # Vehicle to move
+    ego_position = (-80.0, -2.0, ground_level)
+    ego_vehicle = Vehicle('ego', model='etk800', licence='ego', color="white")
+    electrics = Electrics()
+    ego_vehicle.attach_sensor('electrics', electrics)
+    scenario.add_vehicle(ego_vehicle, pos=ego_position, rot=None, rot_quat=direction_of_the_road)
+
+    # Obstacle
+    parked_car_position = (0.0, -2.0, ground_level)
+    parked_vehicle = Vehicle('parked', model='etk800', licence='parked', color="red")
+    scenario.add_vehicle(parked_vehicle, pos=parked_car_position, rot=None, rot_quat=direction_of_the_road)
+
+    # Load all stuffs into BeamNG
+    with BeamNGpy('localhost', 64256, home=BNG_HOME, user=BNG_USER) as bng:
+        bng.set_steps_per_second(60)    #60 fps
+        bng.set_deterministic()
         scenario.make(bng)
         bng.load_scenario(scenario)
-
+        bng.switch_vehicle(ego_vehicle) # Change camera
         bng.start_scenario()
-
-        bng.switch_vehicle(ego_vehicle)
 
         for _ in range (240):
             time.sleep(.1)
@@ -43,4 +46,5 @@ def main():
 
         input("Press enter when done...")
 
-main()
+if __name__ == "__main__":
+    main()
