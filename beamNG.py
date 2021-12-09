6701928177c77efcff8cad5f391b5e58856be183
Youngseok Choe <dev.youngseok@gmail.com>
2021-12-09 22:53:25 +0900

    Remove unnecessary parts

--- beamNG.py
+++ beamNG.py
@@ -1,7 +1,8 @@
 
 from config import BNG_HOME, BNG_USER
 from beamngpy import BeamNGpy, Scenario, Vehicle, Road
-from beamngpy.sensors import Electrics, Damage, State
+from beamngpy.beamngcommon import set_up_simple_logging
+from beamngpy.sensors import Damage
 import math
 import time
 import json
@@ -14,109 +15,100 @@ def dist(tup1,tup2):
     bx,by=tup2[0],tup2[1]'''
     return abs(ax-bx)
 
-def run(port, car_name):
+def run(port, car_name, debug=False):
+    if debug:
+        set_up_simple_logging()
+    result={}
+    result['speed'] = 0.0
+    result['intensity']=math.inf
+    result['distance']=math.inf
+
     try:
-        with BeamNGpy('localhost', port, home=BNG_HOME, user=BNG_USER) as bng:
-            scenario = Scenario('tig', 'test_scenario')
-            ground_level = -28.0
-            road_nodes = [
-                    (-100, 0, ground_level, 8),
-                    (  0, 0, ground_level, 8),
-                    (+100, 0, ground_level, 8)
-                ]
-            road = Road('tig_road_rubber_sticky', rid='straight_road')
-            road.nodes.extend(road_nodes)
-            scenario.add_road(road)
-            direction_of_the_road = (0, 0, 1, -1)
-
-            # Vehicle to move
-            ego_position = (-40.0, -2.0, ground_level)
-            ego_vehicle = Vehicle('ego', model=car_name, licence='ego', partConfig=f"vehicles/{car_name}/etk856tc_M.pc")
-            
-
-            electrics = Electrics()
-            damage=Damage()
-            # with open(f"{BNG_USER}\\0.23\\mods\\unpacked\\barstow_modified\\vehicles\\barstow_forEdit\\291m.pc", 'r') as json_file:
-            #     ego_vehicle.set_part_config(json.load(json_file))
-            
-            ego_vehicle.attach_sensor('electrics', electrics)
-            ego_vehicle.attach_sensor('damage', damage)
-            scenario.add_vehicle(ego_vehicle, pos=ego_position, rot=None, rot_quat=direction_of_the_road)
-
-            # Obstacle
-            parked_car_position = (0.0, -2.0, ground_level)
-            parked_vehicle = Vehicle('parked', model='etk800', licence='parked', color="red")
-            scenario.add_vehicle(parked_vehicle, pos=parked_car_position, rot=None, rot_quat=direction_of_the_road)
-
-            # Load all stuffs into BeamNG
+        bng = BeamNGpy('localhost', port, home=BNG_HOME, user=BNG_USER)
+        bng.open()
         
-            bng.set_steps_per_second(60)    #60 fps
-            bng.set_deterministic()
-            scenario.make(bng)
-            bng.load_scenario(scenario)
-            bng.switch_vehicle(ego_vehicle) # Change camera
-            bng.start_scenario()
+        scenario = Scenario('tig', f'test_scenario')
+        ground_level = -28.0
+        road_nodes = [
+                (-100, 0, ground_level, 8),
+                (  0, 0, ground_level, 8),
+                (+100, 0, ground_level, 8)
+            ]
+        road = Road('tig_road_rubber_sticky', rid='straight_road')
+        road.nodes.extend(road_nodes)
+        scenario.add_road(road)
+        direction_of_the_road = (0, 0, 1, -1)
 
-            result={}
+        # Vehicle to move
+        ego_position = (-40.0, -2.0, ground_level)
+        ego_vehicle = Vehicle('ego', model=car_name, licence='ego', partConfig=f"vehicles/{car_name}/etk856tc_M.pc")
+        damage=Damage()
+        ego_vehicle.attach_sensor('damage', damage)
+        scenario.add_vehicle(ego_vehicle, pos=ego_position, rot=None, rot_quat=direction_of_the_road)
 
-            ## Throttle until obstacle is detected.
-            ego_vehicle.control(throttle=0.8)
-            no_problem = True
-            for i in range(10000000):
+        # Obstacle
+        parked_car_position = (0.0, -2.0, ground_level)
+        parked_vehicle = Vehicle('parked', model='etk800', licence='parked', color="red")
+        scenario.add_vehicle(parked_vehicle, pos=parked_car_position, rot=None, rot_quat=direction_of_the_road)
 
-                if i==20 and ego_vehicle.state['vel'][0] < 0.1:
-                    no_problem = False
-                    break
-                sensors = bng.poll_sensors(ego_vehicle)
-                bng.poll_sensors(parked_vehicle)
-                ego_vehicle.update_vehicle()
-                parked_vehicle.update_vehicle()
+        # Load all stuffs into BeamNG
+        bng.set_steps_per_second(60)    #60 fps
+        bng.set_deterministic()
+        scenario.make(bng)
 
-                if(((dist(ego_vehicle.state['pos'], parked_vehicle.state['pos'])-5)/(ego_vehicle.state['vel'][0])**2) <= 0.078):
-                    ## If brake not applied, they will collide. Start braking
-                    print("brake!")
-                    break
+        # Start scenario
+        bng.load_scenario(scenario)
+        bng.switch_vehicle(ego_vehicle) # Change camera
+        bng.start_scenario()
 
-            if not no_problem:
-                result['speed'] = 0.0
-                result['intensity']=math.inf
-                result['distance']=math.inf
-                return result
-            
-            ## Input breaks
+        ## Throttle until obstacle is detected.
+        ego_vehicle.control(throttle=0.8)
+        no_problem = True
+        for i in range(10000000):
+            sensors = bng.poll_sensors(ego_vehicle)
+            ego_pos = sensors['state']['pos']
+            ego_vel = sensors['state']['vel'][0]
+            if i==20 and ego_vel < 0.1:
+                no_problem = False
+                break
+
+            if(((dist(ego_pos, parked_car_position)-5)/ego_vel**2) <= 0.078):
+                ## If brake not applied, they will collide. Start braking
+                break
+        
+        if no_problem:
             ego_vehicle.control(throttle=0,brake=1.0)
             past_speed = None
-            past_dist = None
             while True:
                 sensors = bng.poll_sensors(ego_vehicle)
-                bng.poll_sensors(parked_vehicle)
-                ego_vehicle.update_vehicle()
-                parked_vehicle.update_vehicle()
-                if sensors['damage']['damage'] > 0:
-                    result['distance']=dist(ego_vehicle.state['pos'],parked_vehicle.state['pos'])
+                ego_pos = sensors['state']['pos']
+                ego_vel = sensors['state']['vel'][0]
+                ego_damage = sensors['damage']['damage'] 
+                if ego_damage > 0:
+                    result['distance']=dist(ego_pos, parked_car_position)
                     result['speed'] = past_speed
-                    result['intensity']=sensors['damage']['damage']
+                    result['intensity'] = ego_damage
                     break
-                past_speed = ego_vehicle.state['vel'][0]
+                
+                past_speed = ego_vel
 
-                if ego_vehicle.state['vel'][0] <= 1e-10:
+                if ego_vel <= 1e-10:
                     result['speed'] = 0
-                    result['intensity']=sensors['damage']['damage']
-                    result['distance']=dist(ego_vehicle.state['pos'],parked_vehicle.state['pos'])
+                    result['intensity'] = ego_damage
+                    result['distance'] = dist(ego_pos, parked_car_position)
                     break
+        
+        bng.close()
+    except ConnectionResetError as e:
+        if (debug):
+            print(e)
 
-            ## Record the intensity and final distance
-            bng.stop_scenario()
-            return result
-    
-    except ConnectionResetError:
-        result = {}
-        result['speed'] = 0.0
-        result['intensity']=math.inf
-        result['distance']=math.inf
+    finally:
+        if (debug):
+            print(result)
         return result
         
 # print(run())
 
 if __name__ == "__main__":
-    run(64256, 'car0')
+    run(64256, 'car0', debug=True)
