d541d654164bf1cf2d061ed4760146825cdf50a9
DESKTOP-3VL352F\유호진 <samtiz@naver.com>
2021-12-09 20:22:57 +0900

    velocity-based AEB

--- beamNG.py
+++ beamNG.py
@@ -8,10 +8,11 @@ import json
 
 
 def dist(tup1,tup2):
-    ax,ay=tup1[0],tup1[1]
-    bx,by=tup2[0],tup2[1]
-    return ((ax-bx)**2+(ay-by)**2)**0.5
-
+    ax=tup1[0]
+    bx=tup2[0]
+    '''ax,ay=tup1[0],tup1[1]
+    bx,by=tup2[0],tup2[1]'''
+    return abs(ax-bx)
 
 def run(port, car_name):
     with BeamNGpy('localhost', port, home=BNG_HOME, user=BNG_USER) as bng:
@@ -28,7 +29,7 @@ def run(port, car_name):
         direction_of_the_road = (0, 0, 1, -1)
 
         # Vehicle to move
-        ego_position = (-70.0, -2.0, ground_level)
+        ego_position = (-40.0, -2.0, ground_level)
         ego_vehicle = Vehicle('ego', model=car_name, licence='ego', partConfig=f"vehicles/{car_name}/etk856tc_M.pc")
         
 
@@ -39,8 +40,6 @@ def run(port, car_name):
         
         ego_vehicle.attach_sensor('electrics', electrics)
         ego_vehicle.attach_sensor('damage', damage)
-        # ego_vehicle.attach_sensor('electrics', electrics)
-        # ego_vehicle.attach_sensor('damage', damage)
         scenario.add_vehicle(ego_vehicle, pos=ego_position, rot=None, rot_quat=direction_of_the_road)
 
         # Obstacle
@@ -56,14 +55,14 @@ def run(port, car_name):
         bng.load_scenario(scenario)
         bng.switch_vehicle(ego_vehicle) # Change camera
         bng.start_scenario()
+
         result={}
-        #while True:
-            #print(scenario._get_vehicles_list())
-            ## Throttle until obstacle is detected.
+
+        ## Throttle until obstacle is detected.
         ego_vehicle.control(throttle=0.8)
         no_problem = True
         for i in range(10000000):
-            #time.sleep(.01)
+
             if i==20 and ego_vehicle.state['vel'][0] < 0.1:
                 no_problem = False
                 break
@@ -71,15 +70,13 @@ def run(port, car_name):
             bng.poll_sensors(parked_vehicle)
             ego_vehicle.update_vehicle()
             parked_vehicle.update_vehicle()
-            if(dist(ego_vehicle.state['pos'],parked_vehicle.state['pos'])<18):
-                ## Obstacle is detected
+
+            if(((dist(ego_vehicle.state['pos'],parked_vehicle.state['pos'])-5)/(ego_vehicle.state['vel'][0])**2) <= 0.078):
+                ## If brake not applied, they will collide. Start braking
                 print("brake!")
                 break
-            #if i % 10 == 0:
-            #print(ego_vehicle.state['vel'])
-                #print(sensors)
             
-            if ego_vehicle.state['vel'][0] >= 13.9:
+            '''if ego_vehicle.state['vel'][0] >= 13.9:
                 ego_vehicle.control(throttle=0.07)
             elif ego_vehicle.state['vel'][0] >= 13.8:
                 ego_vehicle.control(throttle=0.08)
@@ -88,7 +85,8 @@ def run(port, car_name):
             elif ego_vehicle.state['vel'][0] >= 13.2:
                 ego_vehicle.control(throttle=0.1)
             else:
-                ego_vehicle.control(throttle=0.8)
+                ego_vehicle.control(throttle=0.8)'''
+
         if not no_problem:
             result['speed'] = 0.0
             result['intensity']=math.inf
@@ -98,18 +96,16 @@ def run(port, car_name):
         ## Input breaks
         ego_vehicle.control(throttle=0,brake=1.0)
         past_speed = None
+        past_dist = None
         while True:
-            #time.sleep(0.1)
             sensors = bng.poll_sensors(ego_vehicle)
             bng.poll_sensors(parked_vehicle)
-            ego_vehicle.control(throttle=0, brake=1.0)
             ego_vehicle.update_vehicle()
             parked_vehicle.update_vehicle()
-
             if sensors['damage']['damage'] > 0:
+                result['distance']=dist(ego_vehicle.state['pos'],parked_vehicle.state['pos'])
                 result['speed'] = past_speed
                 result['intensity']=sensors['damage']['damage']
-                result['distance']=dist(ego_vehicle.state['pos'],parked_vehicle.state['pos'])
                 break
             past_speed = ego_vehicle.state['vel'][0]
 
@@ -122,41 +118,8 @@ def run(port, car_name):
         ## Record the intensity and final distance
         bng.stop_scenario()
         return result
-            #bng.stop_scenario()
-            #scenario.make(bng)
-            
-            # bng.stop_scenario()
-            # # scenario.remove_vehicle(ego_vehicle)
-            # # scenario.add_vehicle(ego_vehicle)
-            # # bng.load_scenario(scenario)
-            # new_scenario = scenario = Scenario('tig', 'test_scenario')
-            # new_scenario.add_road(road)
-            # ego_vehicle = Vehicle('ego', model='barstow_forEdit', licence='ego', color="white")
-            # #ego_vehicle.attach_sensor('state', state)
-            # ego_vehicle.attach_sensor('electrics', electrics)
-            # ego_vehicle.attach_sensor('damage', damage)
-            
-
-            # new_scenario.add_vehicle(ego_vehicle, pos=ego_position, rot=None, rot_quat=direction_of_the_road)
-
-            # new_scenario.add_vehicle(parked_vehicle, pos=parked_car_position, rot=None, rot_quat=direction_of_the_road)
-            # new_scenario.make(bng)
-            # bng.load_scenario(new_scenario)
-            # bng.start_scenario()
-
-            # scenario.restart()
-            # bng.restart_scenario()
-            # bng.switch_vehicle(ego_vehicle)
-
-            
-
-            #bng.load_scenario(scenario)
-            
-            
-            
-            #bng.despawn_vehicle(ego_vehicle)
-            #ego_vehicle = Vehicle('ego', model='barstow_forEdit', licence='ego', color="black")
-            #bng.spawn_vehicle(ego_vehicle, pos = ego_position, rot = None, rot_quat=direction_of_the_road)
+        
+# print(run())
 
 if __name__ == "__main__":
     run(64256, 'car0')
